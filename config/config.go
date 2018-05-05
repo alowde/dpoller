294493974ba7c0c9e2d8b6b175d6b6a4cf3392d8
Ashley Lowde <ashley.lowde@gmail.com>
2018-05-05 13:48:28 +0930

    Refactor config for improved idiomaticity, YAML support

--- config/config.go
+++ config/config.go
@@ -3,53 +3,39 @@
 package config
 
 import (
+	"bytes"
 	"encoding/json"
 	"github.com/Sirupsen/logrus"
 	"github.com/alowde/dpoller/logger"
+	"github.com/ghodss/yaml"
 	"github.com/pkg/errors"
-	"io/ioutil"
+	"io"
 	"net/http"
+	"os"
 )
 
+func logClose(c io.Closer) {
+	if err := c.Close(); err != nil {
+		log.WithError(err).
+			Warn("Somehow failed to close a Closer")
+	}
+}
+
 type configSkeleton struct {
-	Listen    *json.RawMessage `json:"listen-config"`
-	Publish   *json.RawMessage `json:"publish-config"`
-	Alert     *json.RawMessage `json:"alert-config"`
-	Contacts  *json.RawMessage `json:"contacts"`
-	Tests     *json.RawMessage `json:"urls"`
-	ConfigURL string           `json:"config-url"`
+	Listen   *json.RawMessage `json:"listeners"`
+	Publish  *json.RawMessage `json:"publishers"`
+	Alert    *json.RawMessage `json:"alerters"`
+	Contacts *json.RawMessage `json:"contacts"`
+	Tests    *json.RawMessage `json:"urls"`
+	Config   *configDetails   `json:"config"`
 }
 
-// Unparsed holds the collection of raw JSON that are subsequently parsed by other modules.
-var Unparsed configSkeleton
-
-var log *logrus.Entry
-
-// Initialise wraps the two config initialisation methods, static config from a local file and dynamic config from an
-// HTTP server.
-func Initialise(ll logrus.Level) error {
-
-	log = logger.New("config", ll)
-
-	log.Debug("Loading static configuration")
-	if err := staticInitialise(); err != nil {
-		return errors.Wrap(err, "could not initialise static config")
-	}
-	log.Debug("Loading http configuration")
-	if err := httpInitialise(); err != nil {
-		return errors.Wrap(err, "could not initialise http config")
-	}
-	return nil
+type configDetails struct {
+	ConfigURL string `json:"url"`
+	ConfigKey string `json:"key"`
 }
 
-func staticInitialise() error {
-	raw, err := ioutil.ReadFile("./config.json")
-	if err != nil {
-		return errors.Wrap(err, "could not read config file")
-	}
-	if err = json.Unmarshal(raw, &Unparsed); err != nil {
-		return errors.Wrap(err, "could not parse config file")
-	}
+func (c *configSkeleton) Validate() error {
 	if Unparsed.Listen == nil {
 		return errors.New("undefined listen block")
 	} else if Unparsed.Publish == nil {
@@ -59,20 +45,69 @@ func staticInitialise() error {
 	} else if Unparsed.Contacts == nil {
 		return errors.New("undefined contacts block")
 	}
-	log.WithField("configJson", &Unparsed).Debug("Loaded configuration file")
 	return nil
 }
 
-func httpInitialise() error {
-	log.Debug("Loading http configuration")
-	res, err := http.Get(Unparsed.ConfigURL)
-	if err != nil {
-		return errors.Wrap(err, "couldn't read data from Config URL")
+func (c *configSkeleton) Load(r io.Reader) error {
+	var buf bytes.Buffer
+	if _, err := buf.ReadFrom(r); err != nil {
+		return errors.Wrap(err, "failed to read config data from provided io.Reader")
 	}
-	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
-	if err := json.Unmarshal(body, &Unparsed); err != nil {
-		return errors.Wrap(err, "couldn't parse data from Config URL")
+	if err := yaml.Unmarshal(buf.Bytes(), c); err != nil {
+		log.WithField("config data", buf.String()).
+			Debug("Failed to parse provided config data")
+		return errors.Wrap(err, "failed to parse provided config data")
 	}
 	return nil
+}
+
+// Unparsed holds the collection of raw JSON that are subsequently parsed by other modules.
+var Unparsed = configSkeleton{}
+
+var log *logrus.Entry
+
+// Initialise retrieves static config from local file(s) and dynamic config from an HTTP server.
+func Initialise(ll logrus.Level) error {
+
+	log = logger.New("config", ll)
+
+	// Attempt to open and merge config from each of the provided file names
+	log.Debug("Loading file configuration")
+	// TODO: support runtime-defined config name
+	confNames := [3]string{"config.json", "config.yaml", "config.yml"}
+	for _, name := range confNames {
+		file, err := os.Open(name)
+		if err != nil {
+			log.WithError(err).
+				WithField("file", name).
+				Debug("couldn't read config file")
+			continue
+		}
+		if err := Unparsed.Load(file); err != nil {
+			log.WithError(err).
+				WithField("file", name).
+				Warn("couldn't parse config file")
+		}
+	}
+
+	// Attempt to receive and merge config from the HTTP URL, if any
+	log.Debug("Loading http configuration")
+	if Unparsed.Config.ConfigURL != "" {
+		res, err := http.Get(Unparsed.Config.ConfigURL)
+		if err != nil {
+			log.WithError(err).
+				WithField("url", Unparsed.Config.ConfigURL).
+				Warn("couldn't read config from URL")
+			return Unparsed.Validate()
+
+		}
+		defer logClose(res.Body)
+		if err := Unparsed.Load(res.Body); err != nil {
+			log.WithError(err).
+				WithField("url", Unparsed.Config.ConfigURL).
+				Warn("couldn't parse config from URL")
+			return Unparsed.Validate()
+		}
+	}
+	return Unparsed.Validate()
 }
